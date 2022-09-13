ca50fbd13136141897e4b433626e73273d4891b5
Fernando Camacho <fernandocamacho@Fernandos-MacBook-Pro.local>
2022-09-12 21:31:43 -0500

    variousBinary fix

--- binarySearch/variousBinary.cpp
+++ binarySearch/variousBinary.cpp
@@ -27,23 +27,28 @@ using namespace std;
 //     return 0;
 // }
 
-void binarySearch(int num, vector<int> v){
-    int l = 0, 
-        r = v.size() - 1, 
-        mid = 0;
+void binaryStride(int num, vector<int> a){
+    int sz = a.size();
     
-    while(l + 1 < r){
-        mid = (l + r) / 2;
+    int pow = log2(sz);
+    int pos = 0;
+    int paso = 0;
 
-        if(num > v[mid]){
-            r = mid;
+    for(int i = pow; i >= 0; i--){
+        paso = 1 << i; //pow^i
+        if(pos + paso <= sz){ // el paso es menor al largo del arreglo
+            if(num > a[paso + pos - 1]){
+                pos += paso;
+            }
         }
-        else{
-            l = mid + 1;
-        }
-        
     }
 
+    if(a[pos] == num){
+        cout << 1 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
 }
 
 int main(){
